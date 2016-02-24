4ce96169bedc83321f087115f6782138a95314c5
Harrison Erd <patx@patxbot.tk>
2016-02-24 04:33:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 pickleDB
 --------
 
-pickleDB is lightweight, fast, and simple database based on Python's own 
+pickleDB is lightweight, fast, and simple database based on Python's own
 json module. And it's BSD licensed!
 
 pickleDB is Fun
@@ -35,24 +35,24 @@ Links
 
 * `website <http://packages.python.org/pickleDB/>`_
 * `documentation <http://packages.python.org/pickleDB/commands.html>`_
-* `github repo
-  <https://github.com/patx/pickledb>`_
+* `bitbucket repo <https://bitbucket.org/patx/pickledb>`_
 
 """
 
 from distutils.core import setup
 
 setup(name = "pickleDB",
-    version="0.3",
-    description="A lightweight and simple database using json.",
+    version="0.6.2",
+    description="A lightweight and simple database using simplejson.",
     author="Harrison Erd",
     author_email="patx44@gmail.com",
     license="three-clause BSD",
-    url="http://github.com/patx/pickledb",
+    url="http://bitbucket.org/patx/pickledb",
     long_description=__doc__,
     classifiers = [
         "Programming Language :: Python",
         "License :: OSI Approved :: BSD License",
         "Intended Audience :: Developers",
         "Topic :: Database" ],
-    py_modules=['pickledb'])
+    py_modules=['pickledb'],
+    install_requires=['simplejson'])
