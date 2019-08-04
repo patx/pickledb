e3933c99a99b5112db697e19bbeb98afaf70d090
Pieterjan Montens <pieterjan@montens.net>
2019-08-04 17:44:30 +0200

    Allow loading of currently empty files
    In a testing environment, `tempfile.mkstemp` generates temporary empty files, automatically cleaned after test execution.
    This modification allow pickledb to use these files, without hacks like writing random JSON in the generated file.

--- pickledb.py
+++ pickledb.py
@@ -99,7 +99,13 @@ class PickleDB(object):
         
     def _loaddb(self):
         '''Load or reload the json info from the file'''
-        self.db = json.load(open(self.loco, 'rt'))
+        try: 
+            self.db = json.load(open(self.loco, 'rt'))
+        except ValueError:
+            if os.stat(self.loco).st_size == 0  # Error raised because file is empty
+                self.db = {}
+            else:
+                raise  # File is not empty, avoid overwriting it
 
     def _autodumpdb(self):
         '''Write/save the json dump into the file if auto_dump is enabled'''
